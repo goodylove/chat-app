4e934b59eee4aba98cec0a5b3cf98bce90787054
goodness <goodyc474@gmail.com>
2023-08-26 10:53:11 +0100

    add navbar

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,5 +1,36 @@
+import GoogleButton from "react-google-button";
+
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { auth } from "./../../firebase/index";
+import { useAuthState } from "react-firebase-hooks/auth";
+
 function NavBar() {
-  return <div>navbar</div>;
+  const [user, loading, error] = useAuthState(auth);
+
+  const signInWithGoogle = () => {
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(auth, provider);
+    console.log(user);
+  };
+  const signOut = () => {
+    auth.signOut();
+  };
+  return (
+    <nav className="flex w-full justify-between px-3 bg-black py-3 items-center">
+      <div className="text-2xl font-bold text-green-300">CHAT APP</div>
+
+      {user ? (
+        <button
+          onClick={signOut}
+          className="bg-white p-2 rounded-sm text-md font-bold"
+        >
+          Sign Out
+        </button>
+      ) : (
+        <GoogleButton style={{ width: "153px" }} onClick={signInWithGoogle} />
+      )}
+    </nav>
+  );
 }
 
 export default NavBar;
--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,17 +1,21 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.REACT_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
+  apiKey: "AIzaSyDFbnx7g9jURhrSyZUr6veloCJ5Do6wRrI",
+  authDomain: "chat-2-app-710f0.firebaseapp.com",
+  projectId: "chat-2-app-710f0",
+  storageBucket: "chat-2-app-710f0.appspot.com",
+  messagingSenderId: "530869342960",
+  appId: "1:530869342960:web:bb8b9fc1fce97e6883a835",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const auth = getAuth();
+export const db = getFirestore(app);
